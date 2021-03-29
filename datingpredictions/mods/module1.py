c948d88f66ddf6115d7599e39e88935e013bd224
amiruldevkun <78654637+amiruldevkun@users.noreply.github.com>
2021-03-29 16:49:54 +0800

    UPDATED: done doing some test.
    P/S: I was using a macbook while trying to update the app, so ignore the macenvedit.py and anything that has to do with mac :)

---
+++ datingpredictions/mods/module1.py
@@ -0,0 +1,20 @@
+# Module num1
+
+boy = {
+    "name" : "",
+    "age" : "",
+    "personality" : ["neuroticism", "openness", "agreeableness", "extraversion", "conscientiousness"],
+    "gender" : ""
+}
+
+girl = {
+    "name" : "",
+    "age" : "",
+    "personality" : ["neuroticism", "openness", "agreeableness", "extraversion", "conscientiousness"],
+    "gender" : ""
+}
+# neuroticism = sadge/badmood
+# openness = curious/adventurer
+# agreeableness = trustworthy/helpful
+# extraversion = extroverts
+# conscientiousness = diciplined/act dutifully
